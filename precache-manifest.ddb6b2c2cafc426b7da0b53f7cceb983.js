927abeceb5bb8a2211a7ec9536c7da5868b8fa68
unknown <mohammad127sh82@gmail.com>
2020-05-18 22:01:25 +0430

    Updates

---
+++ precache-manifest.ddb6b2c2cafc426b7da0b53f7cceb983.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b065f8660c622bf4d610461ba2019fc",
+    "url": "/responsive/index.html"
+  },
+  {
+    "revision": "a65c21566bb5d785dcfc",
+    "url": "/responsive/static/css/main.459e91d1.chunk.css"
+  },
+  {
+    "revision": "55ea009973318be1e21f",
+    "url": "/responsive/static/js/2.6220841a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/responsive/static/js/2.6220841a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65c21566bb5d785dcfc",
+    "url": "/responsive/static/js/main.bd256b27.chunk.js"
+  },
+  {
+    "revision": "b04e7f133cbde9638a8e",
+    "url": "/responsive/static/js/runtime-main.b3ef627b.js"
+  },
+  {
+    "revision": "df5c7a4a9b3edc4ae79a50d34b936abc",
+    "url": "/responsive/static/media/avatar_g2.df5c7a4a.jpg"
+  },
+  {
+    "revision": "abf3e5bd50ddb967672a4f6073fe1bd8",
+    "url": "/responsive/static/media/chef.abf3e5bd.jpg"
+  },
+  {
+    "revision": "5397831ef0146722ad189648cb12d237",
+    "url": "/responsive/static/media/gondol.5397831e.jpg"
+  },
+  {
+    "revision": "5ae99eaa2f2dc1f794226fbf00b9665e",
+    "url": "/responsive/static/media/lights.5ae99eaa.jpg"
+  },
+  {
+    "revision": "56d0fbea88ee161bebec22bbcd4d6d76",
+    "url": "/responsive/static/media/man_smoke.56d0fbea.jpg"
+  },
+  {
+    "revision": "89702d61bf192588b4e277579c2509b6",
+    "url": "/responsive/static/media/map.89702d61.jpg"
+  },
+  {
+    "revision": "60d77ba7192b3fc4a10aa14c0d4ebdbf",
+    "url": "/responsive/static/media/mountains.60d77ba7.jpg"
+  },
+  {
+    "revision": "4c139500d9937628ee0baa14a497d237",
+    "url": "/responsive/static/media/nature.4c139500.jpg"
+  },
+  {
+    "revision": "9f16ce879185b606671e7f1b35bc44a7",
+    "url": "/responsive/static/media/newyork.9f16ce87.jpg"
+  },
+  {
+    "revision": "435980365597328e86b0769bdea5613c",
+    "url": "/responsive/static/media/p1.43598036.jpg"
+  },
+  {
+    "revision": "8efb2ccd21aad61f6f7f4da7c7e04988",
+    "url": "/responsive/static/media/p2.8efb2ccd.jpg"
+  },
+  {
+    "revision": "2d8a71ee0879f36303edf5879c63561b",
+    "url": "/responsive/static/media/p3.2d8a71ee.jpg"
+  },
+  {
+    "revision": "c88d58a385dac9c446a2077f4f4406b4",
+    "url": "/responsive/static/media/p6.c88d58a3.jpg"
+  },
+  {
+    "revision": "cec24e47f8a1d1cfce3134bf7aa52578",
+    "url": "/responsive/static/media/paris.cec24e47.jpg"
+  },
+  {
+    "revision": "2ccfdbb0a4abc77ba0a311681ba54981",
+    "url": "/responsive/static/media/pexels-photo-63703.2ccfdbb0.jpg"
+  },
+  {
+    "revision": "44d5624d50026396bb1fda899417f71e",
+    "url": "/responsive/static/media/rocks.44d5624d.jpg"
+  },
+  {
+    "revision": "0c2bad64aa0522f86d24bafdb6b90902",
+    "url": "/responsive/static/media/sailboat.0c2bad64.jpg"
+  },
+  {
+    "revision": "f0140a2e93bf162a2c35491b453ed47d",
+    "url": "/responsive/static/media/sanfran.f0140a2e.jpg"
+  },
+  {
+    "revision": "7d721c29bad496bc5d82aafed68df35e",
+    "url": "/responsive/static/media/underwater.7d721c29.jpg"
+  },
+  {
+    "revision": "fe6ac03e1abea14063c688a0c635ed01",
+    "url": "/responsive/static/media/wedding (1).fe6ac03e.jpg"
+  },
+  {
+    "revision": "be4d5fb44afde714f9d5dedb96bb477f",
+    "url": "/responsive/static/media/workshop.be4d5fb4.jpg"
+  }
+]);
